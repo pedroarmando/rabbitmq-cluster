89075741b6880e15d5015c0bc58dac99a2bc7aa3
Pedro Perez <pedro.armando.perez.fernandez@moskitos.fr>
2019-05-09 17:41:28 +0200

    Remove image before build.

--- _.ps1
+++ _.ps1
@@ -1,5 +1,9 @@
 function up { docker stack deploy --compose-file docker-compose.yml rabbitmq }
 function down { docker stack rm rabbitmq }
-function mk { docker build --tag moskitos.azurecr.io/rabbitmq-node:3.7.13 --file rabbitmq-node.Dockerfile .\context }
+function mk 
+{ 
+    docker rmi -f moskitos.azurecr.io/rabbitmq-node:3.7.13
+    docker build --tag moskitos.azurecr.io/rabbitmq-node:3.7.13 --file rabbitmq-node.Dockerfile .\context 
+}
 function d { docker ps -a }
 function prune { docker container prune --force }
